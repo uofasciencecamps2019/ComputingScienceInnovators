e4571ce036b282ec02e5f7378fa933d65839761c
jsmith46 <40039701+jsmith46@users.noreply.github.com>
2019-06-12 12:43:52 -0600

    Create Palindrome.py

---
+++ Python-master/Python-master/intermediate/Palindrome.py
@@ -0,0 +1,16 @@
+"""
+File: Palindrome.py
+Name:
+    
+Python program that detects whether 
+the given word is a palindrome or not.
+Concepts covered: Strings, loops, slicing, if/else
+"""
+def main():
+    #Enter Code Here
+def Palindrome(word):
+    #Enter Code Here
+
+
+if __name__ == "__main__":
+    main()
