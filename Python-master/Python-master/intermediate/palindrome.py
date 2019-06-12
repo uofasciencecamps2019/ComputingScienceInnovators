d9dccad06ae680757c48cd2d8ac23ff595c62d91
jsmith46 <40039701+jsmith46@users.noreply.github.com>
2019-06-12 12:44:31 -0600

    Update and rename Palindrome.py to palindrome.py

--- Python-master/Python-master/intermediate/palindrome.py
+++ Python-master/Python-master/intermediate/palindrome.py
@@ -8,9 +8,9 @@ Concepts covered: Strings, loops, slicing, if/else
 """
 def main():
     #Enter Code Here
+    
 def Palindrome(word):
     #Enter Code Here
-
 
 if __name__ == "__main__":
     main()
