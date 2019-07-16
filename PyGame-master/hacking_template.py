4241ebd82a5531d7da541b043f2a3a70b04d7e5d
arumbles <rumbles@ualberta.ca>
2019-07-16 15:46:25 -0600

    Add files via upload

---
+++ PyGame-master/hacking_template.py
@@ -0,0 +1,41 @@
+# import
+
+
+# create window
+window = Window()
+window.set_font_color()
+window.set_font_name()
+window.set_bg_color()
+window.set_font_size()
+
+# initialize variables
+
+
+# display remaining attempts
+window.draw_string()
+window.update()
+sleep()
+
+# display password list (similar to above, copy/paste for each password)
+
+
+# prompt user for password
+
+
+# clear window
+window.clear()
+
+# create outcome
+
+
+sleep(2)
+
+# clear window
+window.clear()
+
+# prompt for end
+window.input_string()
+
+# close window
+window.close()
+
