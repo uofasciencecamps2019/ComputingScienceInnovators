88e29d6062552551a5209885b4803aed13dfb7ab
jsmith46 <40039701+jsmith46@users.noreply.github.com>
2019-07-25 10:54:50 -0600

    Update Game.py

--- PyGame-master/Game.py
+++ PyGame-master/Game.py
@@ -1,10 +1,3 @@
-
-
-# -*- coding: utf-8 -*-
-
-
-
-
 #import modules
 
 import pygame
@@ -22,25 +15,44 @@ import time
 #def generate_snake(snake_head, snake_position, button_direction):
 
     #uses button_direction to decide where snake head will go
-
+    #if button_direction == 0:
+        #snake_head[0] += 10
+    #elif button_direction == 1:
+        #snake_head[0] -= 10 
+    #elif button_direction == 2:
+        #snake_head[1] += 10 
+    #elif button_direction == 3:
+        #snake_head[1] -= 10         
+    
 
 #def display_snake(snake_position):
 
     #uses list of snake's positions to display snake
 
-#def play_game(snake_head, snake_position, button_direction):
+def play_game(snake_head, snake_position, button_direction):
 
-    #crashed = False
+    crashed = False
     
-    #while crashed is not True:
+    while crashed is not True:
 
-        #for event in pygame.event.get():
+        for event in pygame.event.get():
 
             #ends game if you click on X
+            if event.type == pygame.QUIT:
+                crashed = True
 
             #sets variable used to move snake using arrow keys
-
-
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    button_direction = 0
+                elif event.key == pygame.K_LEFT:
+                    button_direction = 1
+                elif event.key == pygame.K_DOWN:
+                    button_direction = 2
+                elif event.key == pygame.K_UP:
+                    button_direction = 3                
+                
+            
         #moves snake position
 
         
@@ -100,7 +112,7 @@ if __name__ == "__main__":
 
     #start the game loop
 
-    #play_game(snake_head, snake_position, 1)
+    play_game(snake_head, snake_position, 1)
 
 
 
